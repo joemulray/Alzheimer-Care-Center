e6ccbb09508dd3f84ba5a6e6494309afa6817a31
Joseph Mulray <joe.mulray@gmail.com>
2017-03-23 23:10:51 -0400

    add finished querys :octocat:

--- query.sql
+++ query.sql
@@ -29,3 +29,37 @@ join Checkins on id = Checkins.aID
 join Datasheets on id = Datasheets.aID
 where Checkins.aID is not Null
 and Datasheets.aID is not Null;
+
+
+/*select only activities that have been on outings*/
+select Outings.activity from Outings
+join Activities on Outings.activity = Activities.activity
+where Activities.activity is not NULL;
+
+/*select assistants name id where id less 
+3 and order results in descending order*/
+select id, name from Assistants
+where id < 3 
+order by id desc;
+
+
+/*select paitent name id, where name 
+equals Jane doe and in Datasheets and Checkins*/
+select distinct Datasheets.pID, Datasheets.pname           
+from Datasheets
+join Checkins on Datasheets.pID = Checkins.pID
+where Checkins.pname = 'Jane Doe';
+
+/*select id name from patients
+where ids are greater than 2 order by asc*/
+select id, name from Patients
+where id > 2
+order by id asc;
+
+/*select Datasheet id and time from checkins
+where both are not null order by asc*/
+select sheetid, time from Checkins
+where sheetid is not Null 
+and time is not null
+order by sheetid asc;
+
